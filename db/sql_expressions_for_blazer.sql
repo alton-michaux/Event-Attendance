6b83d36f8e3a14ac904c8e3c3da3070765a86fc3
Alton Michaux <alton.michaux@yahoo.com>
2021-09-17 17:43:03 -0400

    <docs>wrote documentation for EventTrackr Reports and Data

--- db/sql_expressions_for_blazer.sql
+++ db/sql_expressions_for_blazer.sql
@@ -1,11 +1,32 @@
 -- querie to render the event attendee per event chart
 
+<<<<<<< Updated upstream
  SELECT "description" AS "Event", "event_location" AS "Location", COUNT("member_ids") as "Attendees"
  FROM "events" AS "Events" INNER JOIN "events_members" 
  ON "Events"."id" = "events_members"."event_id"
  WHERE "events_members"."member_id" <> "member_ids"
  GROUP BY 1
  ORDER BY "events"."created_at" ASC
+=======
+-- inner join with events_members table
+
+SELECT "description" AS "Event", "event_location" AS "Location", COUNT("member_ids") as "Attendees"
+FROM "events" INNER JOIN "events_members" 
+ON "event_id" = "events_members"."event_id"
+WHERE "events_members"."member_id" <> "member_ids"
+GROUP BY 1
+ORDER BY "events"."created_at" ASC
+
+-- inner join with members table
+
+SELECT "description" AS "Activity", "event_location" AS "Location", COUNT('members') AS "Attendees"
+FROM "events"
+INNER JOIN "members" 
+ON "event_ids" <> "events"."id"
+WHERE 'id' <> 'member_ids'
+GROUP BY 1
+ORDER BY "events"."created_at" ASC
+>>>>>>> Stashed changes
 
 -- querie to render total number of users
 
