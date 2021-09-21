f5cb442c4db9a77cbd5e2d7b41bf0ebd4a972e58
Alton Michaux <alton.michaux@yahoo.com>
2021-09-21 18:24:45 -0400

    <data>corrected sql expression for blaze db, now shows correct number of attendees

--- db/sql_expressions_for_blazer.sql
+++ db/sql_expressions_for_blazer.sql
@@ -3,21 +3,11 @@
 -- inner join with events_members table
 
 SELECT "description" AS "Event", "event_location" AS "Location", COUNT("member_ids") as "Attendees"
-FROM "events" INNER JOIN "events_members" 
-ON "event_id" = "events_members"."event_id"
+FROM "events" AS "E" INNER JOIN "events_members" 
+ON "E"."id" = "events_members"."event_id"
 WHERE "events_members"."member_id" <> "member_ids"
 GROUP BY 1
-ORDER BY "events"."created_at" ASC
-
--- inner join with members table
-
-SELECT "description" AS "Event", "event_location" AS "Location", COUNT('member_ids') AS "Attendees"
-FROM "events"
-INNER JOIN "members" 
-ON "event_ids" <> "events"."id"
-WHERE 'id' <> "member_ids"
-GROUP BY 1
-ORDER BY "events"."created_at" ASC
+ORDER BY "E"."created_at" ASC
 
 -- querie to render total number of users
 
